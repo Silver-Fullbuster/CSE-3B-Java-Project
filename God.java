077ea757c1d2c2716e5ec6a9398946cdc8f4f96f
Kartikeya Chauhan <30721713+Erscheinung@users.noreply.github.com>
2018-11-11 14:59:52 +0530

    Create God.java
    Test Class

---
+++ God.java
@@ -0,0 +1,40 @@
+package chess;
+
+import static chess.Chess.fillChessboard;
+import static chess.Chess.move;
+import static chess.Chess.printBoard;
+import java.util.Scanner;
+
+/**
+ *
+ * @author kartikeyachaauhan
+ */
+public class God {                                                                  
+    	// MAIN METHOD
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Chess.Pieces[][] chessboard = new Chess.Pieces[8][8]; // creates chessboard
+		String move = "";
+                
+         //       System.out.println("Enter side: (w/b)");
+           //     String sidecolour=sc.next();
+   //             selectSide(chessboard, sidecolour);       //will rotate side before fillChessboard fills everything (or can add that functionality in fillChessboard)
+		fillChessboard(chessboard);
+                // Introduce Functionality for entering difficulty: will take game repositories from championship matches and simulate them
+		System.out.println("Input the moves in Standard Algebraic Notation (example: a2 to a3 for leftmost white pawn)”:\n");
+               // Introduce Functionality to re-assign valid moves (for creative game modes)
+               // Introduce hints functionality
+		while(true){                            //condition to keep playing
+			printBoard(chessboard);
+			move = sc.nextLine();
+			
+			if (move.equals("exit")){
+				System.exit(1);
+			}
+			
+			move(chessboard, move);
+						
+		}
+		
+	}
+}
