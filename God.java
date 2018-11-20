6db074723705f4f1f9d274552b909bd95027736e
Kartikeya Chauhan <30721713+Erscheinung@users.noreply.github.com>
2018-11-20 17:51:37 +0530

    Update God.java

--- God.java
+++ God.java
@@ -1,5 +1,41 @@
 import java.util.Scanner;
 
+class Player{
+
+	String player1,player2;
+
+	Player(){
+		this.player1=player1;
+		this.player2=player2;
+	}
+	public void SetWhite(String player1){		
+		this.player1=player1;
+	}
+	public void SetBlack(String player2){
+		this.player2=player2;
+	}
+} 
+
+class Code{
+	public void command(){
+		System.out.print(
+"\t\t\t\t   _::_          \n" +
+"\t\t\t\t _/____\\_	  	  \n" +
+"\t\t\t\t \\      /        \n" +
+"\t\t\t\t  \\____/         \n" +
+"\t\t\t\t  (____)         \n" +
+"\t\t\t\t   |  |   		  \n" +
+"\t\t\t\t   |__|   		  \n" +
+"\t\t\t\t  /    \\         \n" +
+"\t\t\t\t (______) 		  \n" +
+"\t\t\t\t(________)       \n" +
+"\t\t\t\t/________\\       \n");
+	
+		System.out.println("\"If the king does not lead, how can he expect his subordinates to follow?\"");
+
+	}
+}
+
 class Chess {
 
 	public enum Pieces {
@@ -84,6 +120,7 @@ class Chess {
 			System.out.printf("\u001B[31m"+"%4s"+"\u001B[0m", letter);
 			letter++;
 		}
+		System.out.println(" ");
 	}
 
 	public void fillBoard(Pieces[][] chessboard) {
@@ -152,20 +189,22 @@ class Chess {
 
 	}
 
+	//Printing Error messages instead of Exception Handling because it's all under an infinite While Loop
+
 	public void move(Pieces[][] chessboard, String move) {
-		String[] components = move.split("  ");
+		String[] components = move.split(" ");					//Breaks Standard notation into each character
 		
-		if (components.length > 3){
-			System.err.println("Please provide valid move!");
+		if (components.length > 3){								//For incorrect notation
+			System.err.println("\nPlease provide valid move!\n");
 		}
-		else if ( components[0].length() != 2 || components[2].length() != 2){
-			System.err.println("Please provide valid move!");
+		else if (components[0].length() != 2 || components[2].length() != 2){     //For incomplete notation
+			System.err.println("\nPlease provide valid move!\n");
 		}
-		else if ( components[0].charAt(0) < 'a' || components[0].charAt(0) > 'h' || components[0].charAt(1) < '1' || components[0].charAt(1) > '8' ){
-			System.err.println("Please provide valid move!");
+		else if (components[0].charAt(0) < 'a' || components[0].charAt(0) > 'h' || components[0].charAt(1) < '1' || components[0].charAt(1) > '8' ){
+			System.err.println("\nPlease provide valid move!\n");
 		}
-		else if ( components[2].charAt(0) < 'a' || components[2].charAt(0) > 'h' || components[2].charAt(1) < '1' || components[2].charAt(1) > '8' ){
-			System.err.println("Please provide valid move!");
+		else if (components[2].charAt(0) < 'a' || components[2].charAt(0) > 'h' || components[2].charAt(1) < '1' || components[2].charAt(1) > '8' ){
+			System.err.println("\nPlease provide valid move!\n");
 		}
                 else{
                 // make the move: replace original position with Pieces.
@@ -177,100 +216,108 @@ class Chess {
                     int nRow = Math.abs(components[2].charAt(1) - 49-7);
 		
                     System.out.println(col+ " " + row + " " + nCol + " " + nRow);
-                    if (isValid(chessboard, row, col, nRow, nCol)){
+                    if (isValid(chessboard, row, col, nRow, nCol)){						//Valid moves
                       	chessboard[nRow][+nCol] = chessboard[row][+col];
                         chessboard[row][+col] = Pieces.EMPTY;
                     }
-                    else{
-			System.err.println("Illegal Move");  //AHA
-                    }
-		
+                    else
+						System.err.println("Illegal Move");  //AHA
+
                 }
 	}
 
-	
 	
 	public static Boolean isValid(Pieces[][] chessboard, int row, int col, int nRow, int nCol) {
 
 		switch (chessboard[row][col]) {
 			
-		case BLACK_PAWN:
-			if (chessboard[nRow][nCol] != Pieces.EMPTY && (nCol == col + 1 || nCol == col - 1) ){
-				return true;
-			}
-			else if ( chessboard[nRow][nCol] != Pieces.EMPTY ){
-				return false;
-			}
-			else if ( row + 1 == nRow || (row == 1 && row + 2 == nRow)){
-				return true;
-			}
-			break;
-		case WHITE_PAWN:
-			if (chessboard[nRow][nCol] != Pieces.EMPTY && (nCol == col + 1 || nCol == col - 1) ){
-				return true;
-			}
-			else if ( chessboard[nRow][nCol] != Pieces.EMPTY ){
-				return false;
-			}
-			else if ( row - 1 == nRow || (row == 6 && row - 2 == nRow)){
-				return true;
-			}
-			break;
-		case BLACK_ROOK:
+			case BLACK_PAWN:
+				if (chessboard[nRow][nCol] != Pieces.EMPTY && (nCol == col + 1 || nCol == col - 1) ){
+					return true;
+				}
+				else if ( chessboard[nRow][nCol] != Pieces.EMPTY ){
+					return false;
+				}
+				else if ( row + 1 == nRow || (row == 1 && row + 2 == nRow)){
+					return true;
+				}
+				break;
+
+			case WHITE_PAWN:
+				if (chessboard[nRow][nCol] != Pieces.EMPTY && (nCol == col + 1 || nCol == col - 1) ){
+					return true;
+				}
+				else if ( chessboard[nRow][nCol] != Pieces.EMPTY ){
+					return false;
+				}
+				else if ( row - 1 == nRow || (row == 6 && row - 2 == nRow)){
+					return true;
+				}
+				break;
+
+			case BLACK_ROOK:
+				if ((chessboard[nRow][nCol] == Pieces.WHITE_ROOK ||
+					 chessboard[nRow][nCol] == Pieces.WHITE_KNIGHT ||
+					 chessboard[nRow][nCol] == Pieces.WHITE_BISHOP ||
+					 chessboard[nRow][nCol] == Pieces.WHITE_QUEEN ||
+					 chessboard[nRow][nCol] == Pieces.WHITE_KING || 
+               	                  chessboard[nRow][nCol] == Pieces.EMPTY )
+					 && (nCol == col || nRow == row) ){
+					return true;
+					}
+				break;
+
+			case WHITE_ROOK:
+				if ((chessboard[nRow][nCol] == Pieces.BLACK_ROOK ||
+					 chessboard[nRow][nCol] == Pieces.BLACK_KNIGHT ||
+					 chessboard[nRow][nCol] == Pieces.BLACK_BISHOP ||
+					 chessboard[nRow][nCol] == Pieces.BLACK_QUEEN ||
+					 chessboard[nRow][nCol] == Pieces.BLACK_KING || 
+    	             chessboard[nRow][nCol] == Pieces.EMPTY) // ||
+				//	 chessboard[nRow][nCol] == Pieces.BLACK_PAWN)
+					 && (nCol == col || nRow == row) ){
+						return true;
+				}
+				break;
+
+			case BLACK_KNIGHT:
+			case WHITE_KNIGHT:
+				break;
+
+			case BLACK_BISHOP:
+			case WHITE_BISHOP:
+				break;
+
+			case BLACK_QUEEN:
 				if ((chessboard[nRow][nCol] == Pieces.WHITE_ROOK ||
 				 chessboard[nRow][nCol] == Pieces.WHITE_KNIGHT ||
 				 chessboard[nRow][nCol] == Pieces.WHITE_BISHOP ||
 				 chessboard[nRow][nCol] == Pieces.WHITE_QUEEN ||
 				 chessboard[nRow][nCol] == Pieces.WHITE_KING || 
-                                 chessboard[nRow][nCol] == Pieces.EMPTY )
-				 && (nCol == col || nRow == row) ){
-				return true;
+	                         chessboard[nRow][nCol] == Pieces.EMPTY)
+				 && (nCol == col || nRow == row || nCol == col-1 || 
+	                             nRow == row+1 || nRow == row || nRow == row-1)){
+					return true;
 				}
-			break;
-		case WHITE_ROOK:
-			if ((chessboard[nRow][nCol] == Pieces.BLACK_ROOK ||
-				 chessboard[nRow][nCol] == Pieces.BLACK_KNIGHT ||
-				 chessboard[nRow][nCol] == Pieces.BLACK_BISHOP ||
-				 chessboard[nRow][nCol] == Pieces.BLACK_QUEEN ||
-				 chessboard[nRow][nCol] == Pieces.BLACK_KING || 
-                                 chessboard[nRow][nCol] == Pieces.EMPTY )
-				 && (nCol == col || nRow == row) ){
-				return true;
+				break;
+
+			case WHITE_QUEEN:
+				break;
+
+			case BLACK_KING:
+				if ((chessboard[nRow][nCol] == Pieces.WHITE_ROOK ||
+				 chessboard[nRow][nCol] == Pieces.WHITE_KNIGHT ||
+				 chessboard[nRow][nCol] == Pieces.WHITE_BISHOP ||
+				 chessboard[nRow][nCol] == Pieces.WHITE_QUEEN ||
+				 chessboard[nRow][nCol] == Pieces.WHITE_KING || 
+   	                     chessboard[nRow][nCol] == Pieces.EMPTY)
+				 && (( nCol == col+1 || nCol == col || nCol == col-1 ) && ( nRow == row+1 || nRow == row || nRow == row-1 ))){
+					return true;
 				}
-			break;
-		case BLACK_KNIGHT:
-		case WHITE_KNIGHT:
-			break;
-		case BLACK_BISHOP:
-		case WHITE_BISHOP:
-			break;
-		case BLACK_QUEEN:
-			if ((chessboard[nRow][nCol] == Pieces.WHITE_ROOK ||
-			 chessboard[nRow][nCol] == Pieces.WHITE_KNIGHT ||
-			 chessboard[nRow][nCol] == Pieces.WHITE_BISHOP ||
-			 chessboard[nRow][nCol] == Pieces.WHITE_QUEEN ||
-			 chessboard[nRow][nCol] == Pieces.WHITE_KING || 
-                         chessboard[nRow][nCol] == Pieces.EMPTY)
-			 && (nCol == col || nRow == row || nCol == col-1 || 
-                             nRow == row+1 || nRow == row || nRow == row-1)){
-				return true;
-		}
-		break;
-		case WHITE_QUEEN:
-			break;
-		case BLACK_KING:
-			if ((chessboard[nRow][nCol] == Pieces.WHITE_ROOK ||
-			 chessboard[nRow][nCol] == Pieces.WHITE_KNIGHT ||
-			 chessboard[nRow][nCol] == Pieces.WHITE_BISHOP ||
-			 chessboard[nRow][nCol] == Pieces.WHITE_QUEEN ||
-			 chessboard[nRow][nCol] == Pieces.WHITE_KING || 
-                         chessboard[nRow][nCol] == Pieces.EMPTY)
-			 && (( nCol == col+1 || nCol == col || nCol == col-1 ) && ( nRow == row+1 || nRow == row || nRow == row-1 ))){
-				return true;
-		}
-		break;
-		case WHITE_KING:
-			if ((chessboard[nRow][nCol] == Pieces.BLACK_ROOK ||
+				break;
+
+			case WHITE_KING:
+				if ((chessboard[nRow][nCol] == Pieces.BLACK_ROOK ||
 				 chessboard[nRow][nCol] == Pieces.BLACK_KNIGHT ||
 				 chessboard[nRow][nCol] == Pieces.BLACK_BISHOP ||
 				 chessboard[nRow][nCol] == Pieces.BLACK_QUEEN ||
@@ -285,39 +332,61 @@ class Chess {
                                                            nRow == row-1 ))){
                                                                 return true;
                                                            }
-		break;	
-		case EMPTY:
-			return false;		
+				break;	
+
+			case EMPTY:
+				return false;		
 		}
-		return false;
+
+        return false;
 	
 	}            
 	
 }
 
 public class God{	
+
 	public static void main(String[] args) {
+
+		Code Geass=new Code();
+		Geass.command();
+
 		Scanner sc = new Scanner(System.in);
 		Chess.Pieces[][] chessboard = new Chess.Pieces[8][8]; // creates chessboard
 
 		String move = "";
         Chess chess = new Chess();
 		
-		chess.fillBoard(chessboard);
+		chess.fillBoard(chessboard);         // fills chessboard with pieces
+
+		//Player Entries
+		Player player=new Player();
+		System.out.println("\nEnter player 1 name (white): ");
+		player.SetWhite(sc.next());
+		System.out.println("Enter player 2 name (black): ");
+		player.SetBlack(sc.next());
+
+		final long startTime = System.currentTimeMillis();  //Timer Starts
+
 
 		System.out.println("\nInput the moves in Standard Algebraic Notation (example: a2 to a3 for leftmost white pawn)”\n");
-		
-		while(true){                            //condition to keep playing
+
+		//condition to keep playing		
+		while(true){        
 			chess.printBoard(chessboard);
 			move = sc.nextLine();
 			
 			if (move.equals("exit")){
-				System.exit(1);
+				break;
 			}
 			
 			chess.move(chessboard, move);						
 		
         }
 		
+		final long endTime = System.currentTimeMillis();   //Timer Ends
+		System.out.println("Time taken to complete: " + (endTime - startTime)/1000+ "seconds");
+	
 	}
+
 }
