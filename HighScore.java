642c01652d1f898ad17265a2b236f86592b6f142
Mehul Mittal <mehulmittal2010@gmail.com>
2018-11-20 16:30:44 +0530

    Add Score and HighScore class

---
+++ HighScore.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class HighScore {
+    private ArrayList<Score> scoreList = new ArrayList<>();
+
+    public void addScore(Score score) {
+        scoreList.add(score);
+        scoreList.sort(Comparator.comparingInt(Score::getTime));
+    }
+
+    private void displayScoreList() {
+        System.out.println("\tHIGH SCORES\n" +
+                "\n" +
+                "Position\tNAME\tTIME\n");
+        int count = 0;
+        for(Score sc : scoreList)
+            System.out.println(++count + "\t" + sc.getName() +"\t" + sc.getTime());
+    }
+}
---
+++ Score.java
@@ -0,0 +1,17 @@
+public class Score {
+    private String name;
+    private int time;
+
+    public Score(String name, int time) {
+        this.name = name;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTime() {
+        return time;
+    }
+}
