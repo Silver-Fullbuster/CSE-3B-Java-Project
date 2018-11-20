10947a0eb40b0d70d2772179ed7fec3fb4acc3e9
Mehul Mittal <mehulmittal2010@gmail.com>
2018-11-20 18:16:53 +0530

    Delete old TicTacToe

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
-import games.tictactoe.TicTacToe;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
+        HighScore highScores = new HighScore();
         Scanner scanner = new Scanner(System.in);
         System.out.println("1. Tic Tac Toe\n" +
                 "2. Exit");
         if(scanner.nextInt() == 1)
-            TicTacToe.launch();
-
+            TicTacToe.launch(highScores);
+        highScores.displayScoreList();
     }
 }
