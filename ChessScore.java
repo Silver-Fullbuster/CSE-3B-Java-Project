65216a0b7dedd4b0dc17fe64b1b7954b0fabf38d
kartikeyachaauhan <kc1.408596@gmail.com>
2019-04-18 19:33:16 +0530

    fix sql connections for chesscores

--- ChessScore.java
+++ ChessScore.java
@@ -2,11 +2,11 @@ import java.sql.Statement;
 
 class ChessScore extends Score {
 
-	private float elo;
+	private float elo=1200;
 
 	public ChessScore(String name, float time, float elo) {
 		super(name, time);
-		this.elo = elo;
+		this.elo = elo + this.elo;
 	} //Add number of pieces left
 
 	@Override
@@ -18,7 +18,7 @@ class ChessScore extends Score {
 	public void updateDB(int id, Statement statement){
 		super.updateDB(id, statement);
 		String sqlHS = "UPDATE rdbmsproject.highscores SET `Type` = 'Chess' WHERE ID = " + id + ";";
-		String sql = "INSERT INTO rdbmsproject.chessscores VALUES ("  + id + ", " + elo + ");";
+		String sql = "INSERT INTO rdbmsproject.chesscores VALUES ("  + id + ", " + elo + ");";
 		try{
 			statement.executeUpdate(sqlHS);
 			statement.executeUpdate(sql);
@@ -27,4 +27,3 @@ class ChessScore extends Score {
 		}
 	}
 }
-
