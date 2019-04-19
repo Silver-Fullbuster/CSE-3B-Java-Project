0129e27d13ff46463b74eff024349aa6d227110c
kartikeyachaauhan <kc1.408596@gmail.com>
2019-04-19 09:41:18 +0530

    fix react problems

--- Score.java
+++ Score.java
@@ -1,32 +1,50 @@
 import java.sql.*;
 
 public class Score {
-	protected String name;
-	protected float time;
+    protected String name;
+    protected float time;
 
-	public Score(String name, float time) {
-		this.name = name;
-		this.time = time;
-	}
+    public Score(String name, float time) {
+        this.name = name;
+        this.time = time;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public float getTime() {
-		return time;
-	}
+    public float getTime() {
+        return time;
+    }
 
-	public String getScore() {
-		return name + "\t" + time;
-	}
+    public String getScore() {
+        return name + "\t" + time;
+    }
 
-	public void updateDB(int id, Statement statement){
-		String sql = "INSERT INTO rdbmsproject.highscores VALUES ("  + id + ", '" + name + "', " + time + ", 'unknown');";
-		try{
-			statement.executeUpdate(sql);
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-	}
+    public void updateDB(int id, Statement statement) {
+        if (this instanceof ChessScore) {
+            String sql = "INSERT INTO rdbmsproject.highscores VALUES (" + id + ", '" + name + "', " + time + ", 'Chess');";
+            try {
+                statement.executeUpdate(sql);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else if (this instanceof HangmanScore) {
+            String sql = "INSERT INTO rdbmsproject.highscores VALUES (" + id + ", '" + name + "', " + time + ", 'Hangman');";
+            try {
+                statement.executeUpdate(sql);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            String sql = "INSERT INTO rdbmsproject.highscores VALUES (" + id + ", '" + name + "', " + time + ", 'TicTacToe');";
+            try {
+                statement.executeUpdate(sql);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
